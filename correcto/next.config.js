e79aea83bb165fa1311064486082a462aed9755a
Eliemiah <46612897+Eliemiah@users.noreply.github.com>
2022-02-11 11:11:24 -0300

    build && export

--- correcto/next.config.js
+++ correcto/next.config.js
@@ -3,6 +3,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     deviceSizes: [320, 480, 600, 768, 1024, 1200],
+    loader: 'akamai',
+    path: '',
   },
   iframes:{
     path: "https://www.google.com/maps/*"
@@ -28,6 +30,7 @@ module.exports = {
       '/': { page: '/' },
     }
   },
+
 //withPurgeCss
   // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
   //   if (!isServer) {
